1878731d004ef63075db9f9e02b66fe884b2ed4f
oab26 <153092952+oab26@users.noreply.github.com>
2026-01-05 19:19:34 +0500

    Update config.sh to include AWS KMS seal configuration

--- config.sh
+++ config.sh
@@ -1,10 +1,16 @@
 echo "
-{
-  \"storage\": { \"file\": { \"path\": \"${STORAGE_PATH}\" } },
-  \"listener\": [{ \"tcp\": { \"address\": \"[::]:8200\", \"tls_disable\": \"true\" } }],
-  \"default_lease_ttl\": \"${DEFAULT_LEASE_TTL}\",
-  \"max_lease_ttl\": \"${MAX_LEASE_TTL}\",
-  \"ui\": ${UI_ENABLED},
-  \"disable_mlock\": true
-}
-" > config.json
+  {
+    \"storage\": { \"file\": { \"path\": \"${STORAGE_PATH}\" } },
+    \"listener\": [{ \"tcp\": { \"address\": \"[::]:8200\", \"tls_disable\": \"true\" } }],
+    \"seal\": {
+      \"awskms\": {
+        \"region\": \"us-east-1\",
+        \"kms_key_id\": \"alias/vault-unseal\"
+      }
+    },
+    \"default_lease_ttl\": \"${DEFAULT_LEASE_TTL}\",
+    \"max_lease_ttl\": \"${MAX_LEASE_TTL}\",
+    \"ui\": ${UI_ENABLED},
+    \"disable_mlock\": true
+  }
+  " > config.json
